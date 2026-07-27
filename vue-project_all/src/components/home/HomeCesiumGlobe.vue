becc69e1bd49ded7d8b01e33bbefc5831099651f
Libin <2419511891@qq.com>
2026-07-27 15:09:08 +0800

    保存我本地最新代码

--- vue-project_all/src/components/home/HomeCesiumGlobe.vue
+++ vue-project_all/src/components/home/HomeCesiumGlobe.vue
@@ -10546,7 +10546,7 @@ async function triggerRescueMultiAgent() {
   rescueDispatchPending.value = true
   rescueDispatchStatus.value = '救援装备出动中...'
   try {
-    // 强制加载包含协同救援规划路线的 CZML，它内部会调用 api/run_multi_agent 接口
+
     if (typeof loadMission === 'function') {
         await loadMission(true);
     } else {
@@ -10565,7 +10565,6 @@ async function triggerRescueMultiAgent() {
 </script>
 
 <style scoped>
-/* 仿真推演悬浮窗样式 */
 .simulation-popup-panel {
   position: absolute;
   width: 290px;
